f79b5939c6d3d54183deb7b1d6b95c8fc6155600
Arpita <arpitabhat605@gmail.com>
2026-06-06 13:30:48 +0530

    Implement user registration using SQLite

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import sqlite3
 app = Flask(__name__)
+def init_db():
+    conn = sqlite3.connect("database/capsule.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database/capsule.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+            (username, email, password)
+        )
+
+        conn.commit()
+        conn.close()
+
+        return "User Registered Successfully!"
+
     return render_template("register.html")
 
 @app.route("/login")
